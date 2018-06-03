7a3a1a61b09014923b470347e38c6ce01ec80d4a
adamkubarek <adamkubarek@gmail.com>
2018-06-03 16:19:00 +0200

    Spring Boot project initialization

---
+++ src/main/java/pl/javastyle/FitCare/FitCareApplication.java
@@ -0,0 +1,12 @@
+package pl.javastyle.FitCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitCareApplication.class, args);
+	}
+}
