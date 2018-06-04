61a39f2300bc7f9d9a87d797d63e098b37e77487
adamkubarek <adamkubarek@gmail.com>
2018-06-04 20:47:53 +0200

    [New] Added Product&Category domain objects

---
+++ src/main/java/pl/javastyle/FitCare/domain/Category.java
@@ -0,0 +1,29 @@
+package pl.javastyle.FitCare.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Categories")
+@ToString(exclude = "products")
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private Long id;
+    @Getter @Setter
+    private String name;
+    @OneToMany(mappedBy = "category")
+    @Getter @Setter
+    private List<Product> products;
+
+    public boolean isPersisted() {
+        return this.getId() != null;
+    }
+}
---
+++ src/main/java/pl/javastyle/FitCare/domain/Product.java
@@ -0,0 +1,37 @@
+package pl.javastyle.FitCare.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Products")
+@ToString
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private Long id;
+    @Column(unique = true)
+    @Getter @Setter
+    private String name;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Getter @Setter
+    private Category category;
+    @Getter @Setter
+    private double calories;
+    @Getter @Setter
+    private double protein;
+    @Getter @Setter
+    private double carbs;
+    @Getter @Setter
+    private double fat;
+
+    public boolean isPersisted() {
+        return this.getId() != null;
+    }
+}
