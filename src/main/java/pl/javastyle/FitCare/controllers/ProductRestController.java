92c3a67b223fde854ba6e7df7f1783043309f96c
adamkubarek <adamkubarek@gmail.com>
2018-06-04 20:55:37 +0200

    [New] Created REST controller for Product domain

---
+++ src/main/java/pl/javastyle/FitCare/controllers/ProductRestController.java
@@ -0,0 +1,38 @@
+package pl.javastyle.FitCare.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.javastyle.FitCare.controllers.DTO.ProductDTO;
+import pl.javastyle.FitCare.services.interfaces.ProductService;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("api-v1")
+public class ProductRestController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("products")
+    public ResponseEntity addNewProduct (@RequestBody ProductDTO product) {
+        ProductDTO savedProduct = productService.addNewProduct(product);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedProduct.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("products/{productId}")
+    public ResponseEntity updateProduct (@RequestBody ProductDTO product, @PathVariable Long productId) {
+        ProductDTO updatedProduct = productService.updateProduct(product, productId);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+}
