bbc5ebe101f767de4b2409fe520ace02e6861fe8
adamkubarek <adamkubarek@gmail.com>
2018-06-16 14:22:00 +0200

    [Ref] Made error field transient and final
    Sonar hint

--- src/main/java/pl/javastyle/fitcare/exceptions/ApplicationException.java
+++ src/main/java/pl/javastyle/fitcare/exceptions/ApplicationException.java
@@ -1,25 +1,24 @@
 package pl.javastyle.fitcare.exceptions;
 
 import lombok.Getter;
-import lombok.Setter;
 
 public class ApplicationException extends RuntimeException {
 
-    @Getter @Setter
-    private ApplicationError error;
+    @Getter
+    private final transient ApplicationError error;
 
     public ApplicationException(ApplicationError message) {
         super(message.getCode());
-        setError(message);
+        this.error = message;
     }
 
     public ApplicationException(ApplicationError message, Throwable cause) {
         super(message.getCode(), cause);
-        setError(message);
+        this.error = message;
     }
 
     public ApplicationException(ApplicationError message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message.getCode(), cause, enableSuppression, writableStackTrace);
-        setError(message);
+        this.error = message;
     }
 }
