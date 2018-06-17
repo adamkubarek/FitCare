2758bfd9e2d9d78c42e507e8ab2b7d95e4ba711d
adamkubarek <adamkubarek@gmail.com>
2018-06-17 17:07:07 +0200

    [FIT-10][New] Extracted domain-dto mappers to another class

--- src/main/java/pl/javastyle/fitcare/services/ProductServiceImpl.java
+++ src/main/java/pl/javastyle/fitcare/services/ProductServiceImpl.java
@@ -3,12 +3,11 @@ package pl.javastyle.fitcare.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.javastyle.fitcare.rest.dto.ProductDTO;
-import pl.javastyle.fitcare.domain.Category;
 import pl.javastyle.fitcare.domain.Product;
-import pl.javastyle.fitcare.repositories.interfaces.CategoryDAO;
 import pl.javastyle.fitcare.repositories.interfaces.ProductDAO;
+import pl.javastyle.fitcare.rest.dto.ProductDTO;
 import pl.javastyle.fitcare.services.interfaces.ProductService;
+import pl.javastyle.fitcare.services.mappers.ProductMapper;
 
 import java.util.Comparator;
 import java.util.List;
@@ -18,45 +17,29 @@ import java.util.stream.Collectors;
 @Transactional
 public class ProductServiceImpl implements ProductService {
     private ProductDAO productDAO;
-    private CategoryDAO categoryDAO;
+    private ProductMapper mapper;
 
     @Autowired
-    public ProductServiceImpl(ProductDAO productDAO, CategoryDAO categoryDAO) {
+    public ProductServiceImpl(ProductDAO productDAO, ProductMapper mapper) {
         this.productDAO = productDAO;
-        this.categoryDAO = categoryDAO;
+        this.mapper = mapper;
     }
 
     @Override
     public ProductDTO addNewProduct(ProductDTO productDTO) {
-        Product addedProduct = productDAO.saveProduct(mapToProduct(productDTO));
-        return mapToProductDTO(addedProduct);
-    }
-
-    private Product mapToProduct(ProductDTO productDTO) {
-        Product product = new Product();
-        product.setName(productDTO.getName());
-        product.setCalories(productDTO.getCalories());
-        product.setCarbs(productDTO.getCarbs());
-        product.setProtein(productDTO.getProtein());
-        product.setFat(productDTO.getFat());
-        Category category = checkIfCategoryExist(productDTO.getCategory());
-        product.setCategory(category);
-        return product;
-    }
-
-    private Category checkIfCategoryExist(String categoryName) {
-        return categoryDAO.findCategoryByName(categoryName);
+        Product addedProduct = productDAO.saveProduct(mapper.dtoToProduct(productDTO));
+        return mapper.productToDto(addedProduct);
     }
 
     @Override
     public ProductDTO deleteProduct(Product product) {
-        return mapToProductDTO(productDAO.deleteProduct(product.getId()));
+        return mapper.productToDto(productDAO.deleteProduct(product.getId()));
     }
 
     @Override
     public List<ProductDTO> sortAllProductsByCategory() {
         return productDAO.getAllProducts().stream()
-                .map(this::mapToProductDTO)
+                .map(mapper::productToDto)
                 .sorted(Comparator.comparing(ProductDTO::getCategory))
                 .collect(Collectors.toList());
     }
@@ -64,7 +47,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<ProductDTO> sortAllProductsByName() {
         return productDAO.getAllProducts().stream()
-                .map(this::mapToProductDTO)
+                .map(mapper::productToDto)
                 .sorted(Comparator.comparing(ProductDTO::getName))
                 .collect(Collectors.toList());
     }
@@ -72,32 +55,15 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<ProductDTO> sortAllProductsByCalories() {
         return productDAO.getAllProducts().stream()
-                .map(this::mapToProductDTO)
+                .map(mapper::productToDto)
                 .sorted(Comparator.comparing(ProductDTO::getCalories, Comparator.reverseOrder()))
                 .collect(Collectors.toList());
     }
 
     @Override
     public ProductDTO updateProduct(ProductDTO productDTO, Long productId) {
-        Product savedProduct = productDAO.saveProduct(mapToProduct(productDTO, productId));
-        return mapToProductDTO(savedProduct);
-    }
-
-    private Product mapToProduct(ProductDTO productDTO, Long productId) {
-        Product product = mapToProduct(productDTO);
-        product.setId(productId);
-        return product;
-    }
-
-    private ProductDTO mapToProductDTO(Product savedProduct) {
-        ProductDTO productDTO = new ProductDTO();
-        productDTO.setId(savedProduct.getId());
-        productDTO.setCalories(savedProduct.getCalories());
-        productDTO.setCarbs(savedProduct.getCarbs());
-        productDTO.setCategory(savedProduct.getCategory().getName());
-        productDTO.setFat(savedProduct.getFat());
-        productDTO.setName(savedProduct.getName());
-        productDTO.setProtein(savedProduct.getProtein());
-        return productDTO;
+        productDTO.setId(productId);
+        Product savedProduct = productDAO.saveProduct(mapper.dtoToProduct(productDTO));
+        return mapper.productToDto(savedProduct);
     }
 }
--- src/main/java/pl/javastyle/fitcare/services/mappers/ProductMapper.java
+++ src/main/java/pl/javastyle/fitcare/services/mappers/ProductMapper.java
@@ -1,5 +1,9 @@
 package pl.javastyle.fitcare.services.mappers;
 
-public interface ProductMapper {
+import pl.javastyle.fitcare.domain.Product;
+import pl.javastyle.fitcare.rest.dto.ProductDTO;
 
+public interface ProductMapper {
+    Product dtoToProduct(ProductDTO productDTO);
+    ProductDTO productToDto(Product product);
 }
