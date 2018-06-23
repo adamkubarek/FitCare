0152bb8bce951287704d333cdec0bed6f0917067
adamkubarek <adamkubarek@gmail.com>
2018-06-23 16:58:23 +0200

    [Test] Added unit tests for generic domain-dto mapper

---
+++ src/test/java/pl/javastyle/fitcare/services/mappers/ProductMapperTest.java
@@ -0,0 +1,74 @@
+package pl.javastyle.fitcare.services.mappers;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.javastyle.fitcare.domain.Category;
+import pl.javastyle.fitcare.domain.Product;
+import pl.javastyle.fitcare.rest.dto.ProductDTO;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProductMapperTest {
+
+    private Product product;
+    private ProductDTO productDTO;
+
+    private Mapper<Product, ProductDTO> mapper;
+
+    @Before
+    public void setup() {
+        fillProductFields();
+        fillProductDtoFields();
+
+        mapper = new ProductMapper();
+    }
+
+    private void fillProductDtoFields() {
+        productDTO = new ProductDTO();
+        productDTO.setId(2L);
+        productDTO.setName("Ryż");
+        productDTO.setCategory("Kasze i ryże");
+        productDTO.setCarbs(70);
+        productDTO.setProtein(10);
+        productDTO.setFat(10);
+        productDTO.setCalories(410);
+    }
+
+    private void fillProductFields() {
+        product = new Product();
+        product.setId(1L);
+        product.setName("Banan");
+        Category category = new Category();
+        category.setName("Owoce");
+        product.setCategory(category);
+        product.setCarbs(15);
+        product.setProtein(5);
+        product.setFat(10);
+        product.setCalories(170);
+    }
+
+    @Test
+    public void dtoProductShouldHaveTheSameFieldsAsProduct() {
+        Product result = mapper.dtoToDomain(productDTO);
+
+        assertEquals(productDTO.getId(), result.getId());
+        assertEquals(productDTO.getName(), result.getName());
+        assertEquals(productDTO.getCategory(), result.getCategory().getName());
+        assertEquals(productDTO.getCarbs(), result.getCarbs(), 0.001);
+        assertEquals(productDTO.getProtein(), result.getProtein(), 0.001);
+        assertEquals(productDTO.getFat(), result.getFat(), 0.001);
+        assertEquals(productDTO.getCalories(), result.getCalories(), 0.001);
+    }
+
+    @Test
+    public void productShouldHaveTheSameFieldsAsDtoProduct() {
+        ProductDTO result = mapper.domainToDto(product);
+
+        assertEquals(product.getCategory().getName(), result.getCategory());
+        assertEquals(product.getName(), result.getName());
+        assertEquals(product.getCarbs(), result.getCarbs(), 0.001);
+        assertEquals(product.getProtein(), result.getProtein(), 0.001);
+        assertEquals(product.getFat(), result.getFat(), 0.001);
+        assertEquals(product.getCalories(), result.getCalories(), 0.001);
+    }
+}
