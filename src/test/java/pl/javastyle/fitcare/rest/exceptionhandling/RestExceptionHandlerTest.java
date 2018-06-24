e0a6192518d82ab4336ccf8a49ec65b0f11ca22b
adamkubarek <adamkubarek@gmail.com>
2018-06-24 14:19:34 +0200

    [Test] Checking if right status codes are returned when throwing different kind of exceptions

---
+++ src/test/java/pl/javastyle/fitcare/rest/exceptionhandling/RestExceptionHandlerTest.java
@@ -0,0 +1,57 @@
+package pl.javastyle.fitcare.rest.exceptionhandling;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import pl.javastyle.fitcare.exceptions.ApplicationException;
+import pl.javastyle.fitcare.exceptions.DbErrors;
+import pl.javastyle.fitcare.exceptions.ValidationErrors;
+
+import static org.junit.Assert.assertEquals;
+
+public class RestExceptionHandlerTest {
+
+    private RestExceptionHandler handler;
+
+    @Before
+    public void setup() {
+        handler = new RestExceptionHandler();
+    }
+
+    @Test
+    public void shouldReturnBadRequestStatusWhenThrowingValidationError() {
+        try {
+            throw new ApplicationException(ValidationErrors.NOT_VALID);
+        } catch (ApplicationException exception) {
+            ResponseEntity entity = handler.handleApplicationException(exception);
+            HttpStatus result = entity.getStatusCode();
+
+            assertEquals(HttpStatus.BAD_REQUEST, result);
+        }
+    }
+
+    @Test
+    public void shouldReturnBadRequestStatusWhenDuplicatedProductName() {
+        try {
+            throw new ApplicationException(DbErrors.DUPLICATED_PRODUCT_NAME);
+        } catch (ApplicationException exception) {
+            ResponseEntity entity = handler.handleApplicationException(exception);
+            HttpStatus result = entity.getStatusCode();
+
+            assertEquals(HttpStatus.BAD_REQUEST, result);
+        }
+    }
+
+    @Test
+    public void shouldReturnNotFoundStatusWhenProductNotFound() {
+        try {
+            throw new ApplicationException(DbErrors.PRODUCT_NOT_FOUND);
+        } catch (ApplicationException exception) {
+            ResponseEntity entity = handler.handleApplicationException(exception);
+            HttpStatus result = entity.getStatusCode();
+
+            assertEquals(HttpStatus.NOT_FOUND, result);
+        }
+    }
+}
