d9133e9e0df352a9b025c137601822f1a3c9bf95
adamkubarek <adamkubarek@gmail.com>
2018-06-24 13:54:21 +0200

    [Ref] Corrected data.sql after domain changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ INSERT INTO categories(id, name) VALUES (3, 'Warzywa');
 INSERT INTO categories(id, name) VALUES (4, 'Mięsa');
 INSERT INTO categories(id, name) VALUES (5, 'Ryby');
 
-INSERT INTO products(id, calories, carbs, fat, name, protein, category_id) VALUES (1, 100, 10, 10, 'Coca cola', 10, 1);
-INSERT INTO products(id, calories, carbs, fat, name, protein, category_id) VALUES (2, 10, 1, 1, 'Kalafior', 1, 3);
-INSERT INTO products(id, calories, carbs, fat, name, protein, category_id) VALUES (3, 200, 10, 10, 'Banan', 10, 2);
-INSERT INTO products(id, calories, carbs, fat, name, protein, category_id) VALUES (4, 400, 60, 40, 'Ryż Basmati', 10, 1);
+INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id) VALUES (1, 100, 10, 10, 'Coca cola', 10, 1);
+INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id) VALUES (2, 10, 1, 1, 'Kalafior', 1, 3);
+INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id) VALUES (3, 200, 10, 10, 'Banan', 10, 2);
+INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id) VALUES (4, 400, 60, 40, 'Ryż Basmati', 10, 1);
