0e6ad7a6d585f7d00154c470693619dc276097a0
adamkubarek <adamkubarek@gmail.com>
2018-09-08 22:59:30 +0200

    [Ref] Differentiate not found exceptions type

--- src/main/java/pl/javastyle/fitcare/exceptions/DbErrors.java
+++ src/main/java/pl/javastyle/fitcare/exceptions/DbErrors.java
@@ -2,7 +2,9 @@ package pl.javastyle.fitcare.exceptions;
 
 public enum DbErrors implements ApplicationError {
     PRODUCT_NOT_FOUND("Given product doesn't exist"),
+    PRODUCTS_NOT_FOUND("Products not found"),
     CATEGORY_NOT_FOUND("Given category doesn't exist"),
+    CATEGORIES_NOT_FOUND("Categories not found"),
     DUPLICATED_PRODUCT_NAME("Given product already exist"),
     DUPLICATED_CATEGORY_NAME("Given category already exist");
 
