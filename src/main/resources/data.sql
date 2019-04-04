bdeba7a8ea40d2d222b4138084fd117fe4a4f153
adamkubarek <adamkubarek@gmail.com>
2019-04-05 01:53:31 +0200

    [FIT-15][Script] Added script with user role. Removed previous inserts on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,1 @@
-INSERT INTO categories(id, name, audit_cd) VALUES (1, 'Napoje', NOW());
-INSERT INTO categories(id, name, audit_cd) VALUES (2, 'Owoce', NOW());
-INSERT INTO categories(id, name, audit_cd) VALUES (3, 'Warzywa', NOW());
-INSERT INTO categories(id, name, audit_cd) VALUES (4, 'Mięsa', NOW());
-INSERT INTO categories(id, name, audit_cd) VALUES (5, 'Ryby', NOW());
-
-INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id, audit_cd) VALUES (1, 100, 10, 10, 'Coca cola', 10, 1, NOW());
-INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id, audit_cd) VALUES (2, 10, 1, 1, 'Kalafior', 1, 3, NOW());
-INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id, audit_cd) VALUES (3, 200, 10, 10, 'Banan', 10, 2, NOW());
-INSERT INTO products(id, calories, carbohydrates, fat, name, protein, category_id, audit_cd) VALUES (4, 400, 60, 40, 'Ryż Basmati', 10, 1, NOW());
+INSERT INTO roles(id, audit_cd, name) VALUES (1, NOW(), 'ROLE_USER')
